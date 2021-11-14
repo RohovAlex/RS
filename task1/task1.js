7ab6d5a626f3ae7bee71397b5e8c4b6224a94b9b
RohovAlex <47148269+RohovAlex@users.noreply.github.com>
2021-11-14 22:49:02 +0200

    add params check

--- task1/task1.js
+++ task1/task1.js
@@ -6,18 +6,46 @@ const streamsGenerator = require("./modules/streamsGenerator");
 const params = process.argv.splice(2);
 
 if (params.length < 2) {
-  process.stderr.write("Config is missed");
+  process.stderr.write("Config is missed or wrong format");
   process.exit(1);
 }
-console.log(params);
+
+const filterC = params.filter((el) => el === "-c");
+const filterI = params.filter((el) => el === "-i");
+const filterO = params.filter((el) => el === "-o");
+
+if (filterC.length > 1) {
+  process.stderr.write("Config consists duplicated option '-c'");
+  process.exit(1);
+}
+if (filterI.length > 1) {
+  process.stderr.write("Config consists duplicated option '-i'");
+  process.exit(1);
+}
+if (filterO.length > 1) {
+  process.stderr.write("Config consists duplicated option '-o'");
+  process.exit(1);
+}
+
 const config = params[1];
 const configArgs = config.split("-");
 
 const inputFilename = params[3];
 const outputFilename = params[5];
 
-const rs = fs.createReadStream(inputFilename);
-const ws = fs.createWriteStream(outputFilename, { flags: "a+" });
+let rs;
+if (inputFilename) {
+  rs = fs.createReadStream(inputFilename);
+} else {
+  rs = process.stdin;
+}
+
+let ws;
+if (inputFilename) {
+  ws = fs.createWriteStream(outputFilename, { flags: "a+" });
+} else {
+  ws = process.stdout;
+}
 
 const streamsArr = streamsGenerator(configArgs);
 
